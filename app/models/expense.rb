87613d2d646f57c92de1b7d101b748252059b1a7
cpalmer8 <c.elena.palmer@outlook.com>
2015-08-04 18:13:40 +0000

    Add Validation For FK Relationships
    This commit adds validation for the FK relationships associated with the
    expenses model.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,6 @@ class Expense < ActiveRecord::Base
   belongs_to :user
   belongs_to :master_expense
   belongs_to :goal
+  validates :master_expense_id, presence: true
+  validates :goal_id, presence: true
 end
